2057fa70150f51b4019291ce2e1893ea2b69e672
Ananad Rao Gaikwad <123agaikwad@gmail.com>
2022-05-26 19:14:45 +0530

    scanner

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import Add from "./pages/add_invoice/Add";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Product from "./pages/product/Product";
+import Scan from "./pages/qrscan/Scan";
 import Service from "./pages/service/Service";
 
 function App() {
   return (
     <Routes>
+
       <Route path="/" element={<Home/>}/>
       <Route path="/login" element={<Login/>}/>
       <Route path="/product/:id" element={<Product/>}/>
       <Route path="/add_invoice" element={<Add/>}/>
       <Route path="/service_center" element={<Service/>}/>
+      {/* //////////////////////////////////////////////////////////// */}
+
+      <Route path="/scan" element={<Scan/>}/>
+
     </Routes>
   );
 }
---
+++ src/pages/qrscan/Scan.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+import './scan.scss'
+import { Link } from 'react-router-dom'
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import { QrReader } from 'react-qr-reader';
+import img from "../../assets/scanner2.png"
+
+const Scan = () => {
+
+    const [data, setData] = useState("");
+
+    const videoStyle = {
+        width: '350px',
+        borderRadius: '10px',
+        marginLeft: "-47px"
+    }
+
+    const handleError = (err) => {
+        console.log(err);
+    }
+
+    const handleScan = async (result) => {
+
+        try {
+
+            if (result) {
+                console.log(result.text);
+                setData(result.text);
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
+
+
+    return (
+        <>
+            <div className='main'>
+                <div className='scan'>
+                    <div className='header'>
+                        <Link to="/" className='btn back'><ArrowBackIcon fontSize='large' /></Link>
+                    </div>
+                    <p className='heanding'>Scan QR on Product</p>
+
+                    <div className='qrBox'>
+                        <QrReader
+                            onError={handleError}
+                            onResult={handleScan}
+                            videoContainerStyle={videoStyle}
+                        />
+                        <img className='scanImg' src={img} alt="" />
+                    </div>
+                    <p className='instruction'>Align QR inside square</p>
+                    <p className='result'>{data}</p>
+                    <button className='btn manually'>Enter Details Manually</button>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Scan
