342b7597a622ff68024561f45e073f1853937a66
Ananad Rao Gaikwad <123agaikwad@gmail.com>
2022-05-27 12:30:58 +0530

    scanner final

--- src/pages/qrscan/Scan.jsx
+++ src/pages/qrscan/Scan.jsx
@@ -2,12 +2,38 @@ import React, { useState } from 'react'
 import './scan.scss'
 import { Link } from 'react-router-dom'
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import { QrReader } from 'react-qr-reader';
 import img from "../../assets/scanner2.png"
-import BarcodeScannerComponent from "react-qr-barcode-scanner";
 
 const Scan = () => {
 
     const [data, setData] = useState("");
+
+    const videoStyle = {
+        width: '350px',
+        borderRadius: '10px',
+        marginLeft: "-47px"
+    }
+
+    const handleError = (err) => {
+        console.log(err);
+    }
+
+    const handleScan = async (result) => {
+
+        try {
+
+            if (result) {
+                console.log(result.text);
+                setData(result.text);
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
 
 
     return (
@@ -19,21 +45,16 @@ const Scan = () => {
                     </div>
                     <p className='heanding'>Scan QR on Product</p>
 
-                    {/* <div className='qrBox'>
-                        
+                    <div className='qrBox'>
+                        <QrReader
+                            key="environment"
+                            constraints={{ facingMode: 'environment' }}
+                            onError={handleError}
+                            onResult={handleScan}
+                            videoContainerStyle={videoStyle}
+                        />
                         <img className='scanImg' src={img} alt="" />
-                    </div> */}
-
-                    <BarcodeScannerComponent
-                        width={300}
-                        height={300}
-                        onUpdate={(err, result) => {
-                            if (result) setData(result.text);
-                            else setData("Not Found");
-                        }}
-                    />
-
-
+                    </div>
                     <p className='instruction'>Align QR inside square</p>
                     <p className='result'>{data}</p>
                     <button className='btn manually'>Enter Details Manually</button>
