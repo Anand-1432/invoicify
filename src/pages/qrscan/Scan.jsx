c59f2c591d73c2260a3a55e4f5f7a877c549f071
Ananad Rao Gaikwad <123agaikwad@gmail.com>
2022-05-27 11:56:05 +0530

    scanner h

--- src/pages/qrscan/Scan.jsx
+++ src/pages/qrscan/Scan.jsx
@@ -12,7 +12,7 @@ const Scan = () => {
     const videoStyle = {
         width: '550px',
         borderRadius: '10px',
-        marginLeft: "-100px"
+        marginLeft: "-200px"
     }
 
     const handleError = (err) => {
