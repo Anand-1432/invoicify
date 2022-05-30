e6c17d44c00f7a6958377e00a3894bed4c098cca
Ananad Rao Gaikwad <123agaikwad@gmail.com>
2022-05-30 12:11:54 +0530

    updating chart

--- src/pages/dashboard/DashBoard.jsx
+++ src/pages/dashboard/DashBoard.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import './dashBoard.scss'
+import { BarChart, Bar, XAxis, CartesianGrid, Tooltip, LabelList } from 'recharts';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
 import Footer from '../../components/footer/Footer';
 import salepro from '../../assets/salepro.png';
 import Navbar2 from '../../components/navbar2/Navbar2';
 
 const DashBoard = () => {
+
+
+    const data = [
+        { day: 'Mon', amt: 2400 },
+        { day: 'Tue', amt: 2210 },
+        { day: 'Wed', amt: 5290 },
+        { day: 'Thu', amt: 5000 },
+        { day: 'Fri', amt: 3181 },
+        { day: 'Sat', amt: 2500 },
+        { day: 'Sun', amt: 4100 },
+    ];
+
 
 
     const openNavbar = () => {
@@ -31,14 +46,50 @@ const DashBoard = () => {
                         <p>A-One Electronics</p>
                     </div>
 
-                    <div className='chartCon'></div>
+                    <div className='chartCon'>
+
+                     <div className='week'>
+                        <button className='btn btn1'><ArrowBackIosIcon fontSize='small'/></button>
+                         <p>11-17 April</p>
+                        <button className='btn btn2'><ArrowForwardIosIcon fontSize='small'/></button>
+                     </div>
+
+                        {/* ////////////////////////////////////////////////////////////////////////////////////////////////////// */}
+                        <BarChart
+                            className='barCharts'
+                            width={280}
+                            height={160}
+                            data={data}
+                            margin={{top: 0, right: 0, left: 0, bottom:2}}
+                            barSize={12}
+                        >
+
+                            <defs>
+                                <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                                    <stop offset="5%" stopColor="#E54CFE" stopOpacity={1} />
+                                    <stop offset="95%" stopColor="#0F55A7" stopOpacity={1} />
+                                </linearGradient>
+                            </defs>
+
+                            <XAxis dataKey="day" interval={'preserveStartEnd'} scale="point" padding={{ left: 10, right: 10 }} />
+
+                            <Tooltip />
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <Bar dataKey="amt" radius={[4, 4, 0, 0]} fill="url(#colorUv)" background={{ fill: '#eee' }}>
+                                <LabelList dataKey="amt" position="top" fontSize={9} fontWeight={500} />
+                            </Bar>
+                        </BarChart>
+                        {/* ////////////////////////////////////////////////////////////////////////////////////////////////////// */}
+
+
+                    </div>
                     <div className='sellingBrand'></div>
 
                     <div className='companyCon'>
                         <div className='company'></div>
                     </div>
 
-                        <Footer />
+                    <Footer />
                 </div>
             </div>
         </>
